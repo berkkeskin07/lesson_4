24f38f513ca640e7253028bd4b3e37b19dd596bc
berkkeskin07 <berkkeskin0707@gmail.com>
2022-10-11 10:29:30 +0300

    ders içerikleri eklendi

---
+++ strings2.py
@@ -0,0 +1,12 @@
+kelime = "PYTHON" # 'P' 'Y' 'T' 'H' 'O' 'N'
+# Eleman Sayısı : Sayma sayıları ile 1 2 3
+# Index Sayısı : Doğal Sayılarla 0 1 2 3 4 veya tersten -1 -2 -3 -4
+print(kelime)
+print(kelime[0]) # İlk karakteri verir.
+
+print(len(kelime)) # Eleman Sayısı
+print(kelime[5]) # = print(kelime[-1])
+print(kelime[-1])
+
+kelime = "PYTHON DERSİNE HOŞGELDİNİZ" # 5. Elemanın indexi 4'tür. Eleman Sayısı - 1 = INDEX
+print(kelime[len(kelime) - 1])
---
+++ strings3.py
@@ -0,0 +1,10 @@
+kelime = "PYTHON DERSİ"
+# [Başlangıç Index:Bitiş Index:Atlama Değeri]
+
+print(kelime[2:len(kelime)]) # print(kelime[2:])
+print(kelime[2:])
+print(kelime[2:11])
+
+print(kelime[::]) # Tüm Veri
+print(kelime[::2]) # Çift Indexler Geldi
+print(kelime[1:5:2]) #YH
